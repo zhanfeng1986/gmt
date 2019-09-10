fdfc6542034601a253263e2955854573a64492ac
TravisCI <travis@nothing.com>
2019-09-10 15:46:08 +0000

    Deploy dev from TravisCI

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.1.0_93ac779_2019.07.26',
+    VERSION: '6.1.0_58a5b88_2019.09.08',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
