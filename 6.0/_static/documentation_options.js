3514d1d4975c6b8f6f9af7fda74aeae31d618ca6
AzurePipelines <AzurePipelines@nothing.com>
2019-09-12 23:32:27 +0000

    Deploy 6.0 from Azure Pipelines

--- 6.0/_static/documentation_options.js
+++ 6.0/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.0.0_05464c4_2019.09.12',
+    VERSION: '6.0.0_657a97d_2019.09.12',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
