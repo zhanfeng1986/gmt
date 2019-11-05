4052d984fa8a0b0e4cb3ccc039ba2be2d8df394c
AzurePipelines <AzurePipelines@nothing.com>
2019-11-05 22:00:26 +0000

    Deploy 5.4 from Azure Pipelines

--- 5.4/_static/documentation_options.js
+++ 5.4/_static/documentation_options.js
@@ -1,10 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.4.5',
+    VERSION: '5.4.6_c5d65a4_2019.11.05',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
 };
